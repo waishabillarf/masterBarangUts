60b777d2a5a7f9f8c1a7f134d15fbca8311a4f05
waishabillarf <waishabillarf@gmail.com>
2024-12-24 11:16:41 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <hr>
         <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
             <div class="bi-house-fill me-3 fs-1"></div>
-            <h4 class="mb-0">Well done! This is {{ $pageTitle ?? 'Home' }}</h4>
+            <h4 class="mb-0">Sayangnya kecerdasan tidak bisa di download. {{ $pageTitle ?? 'Home' }}</h4>
         </div>
     </div>
 
